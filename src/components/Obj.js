dd1a13fbf6d0a68f83bd20944462dd38348ab7e3
Abdullah-Koroglu <abdullah34koroglu@gmail.com>
2020-03-18 18:32:01 +0300

    Some other basic things been learned.

--- src/components/Obj.js
+++ src/components/Obj.js
@@ -4,35 +4,50 @@ import { useFrame ,extend, useThree } from 'react-three-fiber';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 import {useSpring,a} from 'react-spring/three';
 
-extend(OrbitControls);
+extend({OrbitControls});
+
+
 
 const Controls = () => {
     const orbitRef= useRef();
     const {camera ,gl}=useThree();
 
-    useFrame(()=>{
+    useFrame(state=>{
         orbitRef.current.update()
     });
     return(
         <orbitControls
+        autoRotate
         args={[camera, gl.domElement]}
         ref={orbitRef}
         />
     )
 }
 
+const Plane = () => (
+    <mesh position={[0,-1.5,0]}
+    rotation={[-Math.PI/2,0,0]}>
+    <meshPhysicalMaterial 
+    attach="material" 
+    color="black"/>
+    <planeBufferGeometry
+        attach="geometry"                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+        args={[20,20]}
+    />
+
+    </mesh>
+)
+
 const Box = () => {
     const meshRef= useRef();
     const [hovered ,setHovered] = useState(false)
     const [active ,setActive] = useState(false)
-    const props=useSpring({scale:active ? [1,1,1]:[1.5,1.5,1.5], color:hovered ? "hotpink" : "blue"}
+    const props=useSpring({scale:active ? [1,1,1]:[1.5,1.5,1.5], color:hovered ? "hotpink" : "green"}
  )
- useFrame( () => {
-    meshRef.current.rotation.y += 0.01
-    meshRef.current.rotation.z += 0.02
-  })
+
         return(
             <a.mesh
+            
                 ref={meshRef}
                 onPointerOver={() => setHovered(true)}
                 onPointerOut={ () => setHovered(false)}
@@ -42,15 +57,23 @@ const Box = () => {
                 attach="geometry"
                 args={[2,2,2]}
          />
-            <a.meshBasicMaterial 
+            <ambientLight/>
+            <spotLight position={[0,25,8]}/>
+           
+            <a.meshPhysicalMaterial 
                 attach="material" 
                 color={props.color}/>
             </a.mesh>
 
 )}
 
-
-export default () => (<Canvas>
+// 
+export default () => (
+<div>
+<Canvas camera={ {position :[0,0,5]}}>  
+    <fog attach="fog" args={["white",3,8]} />
+    <Plane/>
     <Controls/>
     <Box/>
-</Canvas>)
+</Canvas>
+    </div>)
